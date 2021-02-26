c0107676d84858dd1fb437ca6e737f6ccc9861c3
Justinas Stankevicius <justinas@justinas.org>
2021-02-26 22:32:04 +0200

    Prototype /crawl endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+const DOC: &str = r#"
+GET /
+
+    this page
+
+POST /crawl
+
+    start crawling
+
+    > {"domain": "http://justinas.org"}
+"#;
+
+#[derive(Deserialize)]
+struct CrawlRequest {
+    domain: url::Url,
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
-    "Hello"
+    DOC
+}
+
+#[post("/crawl")]
+async fn crawl(request: web::Json<CrawlRequest>) -> impl Responder {
+    let domain = request.0.domain;
+    if !matches!(domain.host(), Some(url::Host::Domain(_)))
+        || domain.host_str() == Some("localhost")
+    {
+        return HttpResponse::BadRequest();
+    }
+    HttpResponse::Ok()
 }
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
-    Ok(HttpServer::new(|| App::new().service(index))
+    Ok(HttpServer::new(|| App::new().service(index).service(crawl))
         .bind("127.0.0.1:8080")?
         .run()
         .await?)
